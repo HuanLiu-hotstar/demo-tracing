30d2d6da328d14513c61bd581595c5e9a153d7b9
LIU HUAN <huan.liu>
2021-06-30 20:11:37 +0800

    [update] pc main

---
+++ zipkin/demo/pc/go.mod
@@ -0,0 +1,10 @@
+module github.com/HuanLiu-hotstar/demo-tracing/zipkin/demo/pc
+
+go 1.16
+
+require (
+	github.com/HuanLiu-hotstar/demo-tracing/zipkin/demo/tracelib v0.0.0-20210630102844-69dc8d1bd5c8 // indirect
+	github.com/opentracing-contrib/go-stdlib v1.0.0 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/openzipkin/zipkin-go v0.2.5
+)
---
+++ zipkin/demo/pc/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/HuanLiu-hotstar/demo-tracing/zipkin/demo/tracelib"
+	"github.com/opentracing-contrib/go-stdlib/nethttp"
+	"github.com/opentracing/opentracing-go"
+)
+
+var (
+	serverName = "PC-Server"
+	localAddr  = "192.168.1.63"
+	port       = 18083
+	umAddr     = "http://localhost:18084/um"
+)
+
+func main() {
+
+	f := tracelib.InitTracer(tracelib.WithLocalAddr(localAddr), tracelib.WithLocalName(serverName))
+	defer f()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/pc", pc)
+
+	h := nethttp.Middleware(opentracing.GlobalTracer(), mux)
+	log.Printf("Server listening! %d ...", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), h))
+
+}
+func pc(w http.ResponseWriter, r *http.Request) {
+	span, ctx := tracelib.StartSpanFromContext(r.Context(), r.URL.Path)
+	defer span.Finish()
+	doclient(ctx)
+	x := rand.Intn(100) + 10
+	otherwork(ctx)
+
+	w.Write([]byte(fmt.Sprintf(`{"pc":%d}`, x)))
+}
+func otherwork(c context.Context) {
+	span, _ := tracelib.StartSpanFromContext(c, "other-work")
+	defer span.Finish()
+	x := rand.Intn(100) + 10
+	time.Sleep(time.Duration(x) * time.Millisecond)
+}
+
+func doclient(ctx context.Context) {
+
+	req, err := http.NewRequest("GET", umAddr, nil)
+	if err != nil {
+		log.Printf("err:%s", err)
+		return
+	}
+	req = req.WithContext(ctx)
+	req, ht := nethttp.TraceRequest(opentracing.GlobalTracer(), req)
+	defer ht.Finish()
+	client := http.Client{Timeout: time.Second * 5, Transport: &nethttp.Transport{}}
+	res, err := client.Do(req)
+	if err != nil {
+		log.Printf("err:%s", err)
+		return
+	}
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	log.Printf("body:%s", body)
+
+}
