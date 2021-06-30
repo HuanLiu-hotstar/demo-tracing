69dc8d1bd5c89ee6ff45f41aa37746f62346da0c
LIU HUAN <huan.liu>
2021-06-30 18:28:44 +0800

    [update] um for server

---
+++ zipkin/demo/um/go.mod
@@ -0,0 +1,12 @@
+module github.com/HuanLiu-hotstar/demo-tracing/zipkin/demo/um
+
+go 1.16
+
+require (
+	github.com/HuanLiu-hotstar/demo-tracing/zipkin/demo/tracelib v0.0.0-20210630100303-5f246dfff708
+	github.com/gorilla/mux v1.7.3 // indirect
+	github.com/opentracing-contrib/go-stdlib v1.0.0
+	github.com/opentracing/opentracing-go v1.2.0
+	github.com/openzipkin/zipkin-go v0.2.5
+	google.golang.org/grpc v1.39.0 // indirect
+)
---
+++ zipkin/demo/um/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	// "context"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/HuanLiu-hotstar/demo-tracing/zipkin/demo/tracelib"
+	"github.com/opentracing-contrib/go-stdlib/nethttp"
+	"github.com/opentracing/opentracing-go"
+)
+
+var (
+	// tracer     *openzipkin.Tracer
+	serverName = "UM-Server"
+	localAddr  = "192.168.1.61"
+	port       = 18084
+)
+
+func main() {
+
+	f := tracelib.InitTracer(tracelib.WithLocalAddr(localAddr), tracelib.WithLocalName(serverName))
+	defer f()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/um", um)
+
+	h := nethttp.Middleware(opentracing.GlobalTracer(), mux)
+
+	log.Printf("Server listening! %d ...", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), h))
+
+}
+func um(w http.ResponseWriter, r *http.Request) {
+	span, _ := tracelib.StartSpanFromContext(r.Context(), r.URL.Path)
+	defer span.Finish()
+	x := rand.Intn(100) + 10
+	time.Sleep(time.Duration(x) * time.Millisecond)
+	w.Write([]byte(fmt.Sprintf(`{"um":%d}`, x)))
+}
