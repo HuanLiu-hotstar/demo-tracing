0050f1edba8cb842b91b0799b148a88e4f393d4e
LIU HUAN <huan.liu>
2021-07-06 19:15:15 +0800

    [update] tracelib

--- zipkin/demo/tracelib/go.mod
+++ zipkin/demo/tracelib/go.mod
@@ -3,7 +3,12 @@ module github.com/HuanLiu-hotstar/demo-tracing/zipkin/demo/tracelib
 go 1.16
 
 require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645
+	github.com/opentracing-contrib/go-gin v0.0.0-20201220185307-1dd2273433a4
+	github.com/opentracing-contrib/go-stdlib v1.0.0
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/openzipkin-contrib/zipkin-go-opentracing v0.4.5
 	github.com/openzipkin/zipkin-go v0.2.5
+	google.golang.org/grpc v1.30.0
 )
--- zipkin/demo/tracelib/trace.go
+++ zipkin/demo/tracelib/trace.go
@@ -1,17 +1,26 @@
 package tracelib
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	// "log"
 	// "math/rand"
-	// "net/http"
+	"io/ioutil"
+	"net/http"
 	// "strings"
-	// "time"
+	"time"
 
+	"github.com/gin-gonic/gin"
+	"github.com/grpc-ecosystem/grpc-opentracing/go/otgrpc"
+	"github.com/opentracing-contrib/go-gin/ginhttp"
+	"github.com/opentracing-contrib/go-stdlib/nethttp"
 	"github.com/opentracing/opentracing-go"
+	"google.golang.org/grpc"
+
 	openzipkin "github.com/openzipkin/zipkin-go"
 	//zipkinmw "github.com/openzipkin/zipkin-go/middleware/http"
+
 	zipkinot "github.com/openzipkin-contrib/zipkin-go-opentracing"
 	zipkinHTTP "github.com/openzipkin/zipkin-go/reporter/http"
 )
@@ -68,10 +77,83 @@ func InitTracer(opts ...ConfigOpt) func() {
 	opentracing.SetGlobalTracer(zipkinot.Wrap(nativeTracer))
 	return f
 }
+
+//StartSpanFromContext start a sub-span from context
 func StartSpanFromContext(ctx context.Context, operationName string, opts ...opentracing.StartSpanOption) (opentracing.Span, context.Context) {
 	return opentracing.StartSpanFromContext(ctx, operationName, opts...)
 }
 
+// StartSpan start a new span
 func StartSpan(operationName string, opts ...opentracing.StartSpanOption) opentracing.Span {
 	return opentracing.StartSpan(operationName, opts...)
+}
+
+// NewGrpcConn new client connection to grpc server
+func NewGrpcConn(address string, opt ...grpc.DialOption) (*grpc.ClientConn, error) {
+
+	// Set up a connection to the server.
+	opts := []grpc.DialOption{
+		grpc.WithUnaryInterceptor(otgrpc.OpenTracingClientInterceptor(opentracing.GlobalTracer())),
+		grpc.WithStreamInterceptor(
+			otgrpc.OpenTracingStreamClientInterceptor(opentracing.GlobalTracer())),
+	}
+	opts = append(opts, opt...)
+	conn, err := grpc.Dial(address, opts...)
+	return conn, err
+}
+
+// NewGrpcServer new a grpc server with tracing
+func NewGrpcServer(opt ...grpc.ServerOption) *grpc.Server {
+
+	opts := []grpc.ServerOption{
+		grpc.UnaryInterceptor(otgrpc.OpenTracingServerInterceptor(opentracing.GlobalTracer())),
+		grpc.StreamInterceptor(otgrpc.OpenTracingStreamServerInterceptor(opentracing.GlobalTracer())),
+	}
+	opts = append(opts, opt...)
+	s := grpc.NewServer(opts...)
+	return s
+}
+
+// MiddlewareHttp generate middleware for http.Handler
+func MiddlewareHttp(h http.Handler, options ...nethttp.MWOption) http.Handler {
+	return nethttp.Middleware(opentracing.GlobalTracer(), h, options...)
+}
+
+// MiddlewareGin generate the middleware of gin
+func MiddlewareGin(options ...nethttp.MWOption) func(c *gin.Context) {
+	return ginhttp.Middleware(opentracing.GlobalTracer())
+}
+
+type HttpRequest struct {
+	Method  string
+	Address string
+	Data    []byte
+	Timeout time.Duration
+}
+
+func (h HttpRequest) Do(ctx context.Context, b []byte) ([]byte, error) {
+	return NewHttpReq(ctx, h.Method, h.Address, b, h.Timeout)
+}
+
+// NewHttpReq with context and with a span
+func NewHttpReq(ctx context.Context, method, addr string, bye []byte, timeout time.Duration) ([]byte, error) {
+
+	req, err := http.NewRequest(method, addr, bytes.NewBuffer(bye))
+	if err != nil {
+		return nil, err
+	}
+	req = req.WithContext(ctx)
+	req, ht := nethttp.TraceRequest(opentracing.GlobalTracer(), req)
+	defer ht.Finish()
+	client := http.Client{Timeout: timeout, Transport: &nethttp.Transport{}}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	return body, err
+	//req = req.WithContext(ctx)
+	//req, ht := nethttp.TraceRequest(opentracing.GlobalTracer(), req)
+	//return req, ht
 }
